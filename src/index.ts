256d468f74596869308f9b305a654edae6ba6101
Vladislav Magomedov <vladdmagg94@yandex.ru>
2023-01-22 23:10:56 +0300

    feat: implement ws server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import { WebSocketServer } from 'ws';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = +process.env.PORT! ?? 8100;
+
+const webSocketServer = new WebSocketServer({ port });
+console.log(
+  `Web Socket Server started on port ${webSocketServer.options.port}`,
+);
+
+webSocketServer.on('connection', (ws, req) => {
+  let clientIPAddress = req.socket.remoteAddress;
+  if (clientIPAddress === '::1') {
+    clientIPAddress = 'localhost';
+  }
+  const clientPort = req.socket.remotePort;
+  console.log(
+    `Connection opened by client on ${clientIPAddress}:${clientPort}`,
+  );
+
+  ws.on('message', (data) => {
+    console.log('received: %s', data);
+  });
+});
